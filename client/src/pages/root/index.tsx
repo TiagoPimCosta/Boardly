0b2babd18e6636d185b8941aae5d5d516f33667d
TiagoPimCosta <tiagopimcosta@gmail.com>
2025-10-09 16:29:44 +0100

    fix: hide partners and testimonials

--- client/src/pages/root/index.tsx
+++ client/src/pages/root/index.tsx
@@ -5,20 +5,18 @@ import { Footer } from "./Footer";
 import { Hero } from "./Hero";
 import { HowItWorks } from "./HowItWorks";
 import { Navbar } from "./Navbar";
-import { Partners } from "./Partners";
 import { Pricing } from "./Pricing";
-import { Testimonials } from "./Testimonials";
 
 export const RootPage = () => {
   return (
     <div className="min-h-screen bg-background">
       <Navbar />
       <Hero />
-      <Partners />
+      {/*<Partners />*/}
       <Benefits />
       <HowItWorks />
       <Pricing />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <FAQ />
       <CTA />
       <Footer />
