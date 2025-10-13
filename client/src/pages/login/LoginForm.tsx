0b55632e7594acfeb44f143da89f286754db894f
TiagoPimCosta <tiagopimcosta@gmail.com>
2025-10-13 21:47:26 +0100

    feat: implement login functionality with form validation and routing

---
+++ client/src/pages/login/LoginForm.tsx
@@ -0,0 +1,70 @@
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const loginSchema = z.object({
+  username: z.string().min(2).max(50),
+  password: z.string().min(5).max(18),
+});
+
+type LoginSchema = z.infer<typeof loginSchema>;
+
+export const LoginForm = () => {
+  const form = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(values: LoginSchema) {
+    console.log("Submit Data ", values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input className="text-base" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" className="text-base" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button className="mt-4 w-full" type="submit">
+          Log in
+        </Button>
+      </form>
+    </Form>
+  );
+};
