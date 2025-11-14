f908965bd2cb0a1d41c2d6aeb6310377adcd134a
TiagoPimCosta <tiagopimcosta@gmail.com>
2025-11-14 23:59:54 +0000

    feat: integrate TypeORM with PostgreSQL in app module and update dependencies in package.json

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,11 +4,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ProjectsModule } from './projects/projects.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: process.env.PORT as unknown as number,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      entities: [],
+      database: process.env.DATABASE,
+      synchronize: true,
+      logging: true,
     }),
     AuthModule,
     ProjectsModule,
